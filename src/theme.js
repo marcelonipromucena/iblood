bf60ba70a632ae236963acb7ff92be50fc98d4be
Marceloni Lima <marcelonipromucena@gmail.com>
2020-04-28 11:55:15 -0300

    Project done

---
+++ src/theme.js
@@ -0,0 +1,51 @@
+const colors = {
+  accent: '',
+  primary: '',
+  secondary: '',
+  tertiary: '',
+  black: '',
+  white: '',
+  gray: '',
+  gray2: '',
+};
+
+const sizes = {
+  base: 12,
+  font: 12,
+  border: 12,
+
+  h1: 32,
+  h2: 26,
+  h3: 18,
+  title: 16,
+  body: 12,
+  caption: 12,
+  small: 8,
+};
+
+const fonts = {
+  h1: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: sizes.h1,
+  },
+  h2: {
+    fontSize: sizes.h2,
+  },
+  h3: {
+    fontSize: sizes.h3,
+  },
+  title: {
+    fontSize: sizes.title,
+  },
+  body: {
+    fontSize: sizes.body,
+  },
+  caption: {
+    fontSize: sizes.caption,
+  },
+  small: {
+    fontSize: sizes.small,
+  },
+};
+
+export {colors, sizes, fonts};
